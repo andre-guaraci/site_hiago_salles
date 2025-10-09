d88fb98f23fd120959ff394a777a8b086057e8bc
André Guaraci <guaraci.goncalves81@outlook.com>
2025-10-09 19:30:52 -0300

    Update form_handler.php
    Update no form_handler para tentar mitigar o crash na hora de enviar os dados.

--- helpers/form_handler.php
+++ helpers/form_handler.php
@@ -11,7 +11,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require_once __DIR__ . '/helpers/env_loader.php';
+require_once __DIR__ . '/env_loader.php';
 loadEnv();
 require_once __DIR__ . '/utils.php';
 require_once __DIR__ . '/security.php';
